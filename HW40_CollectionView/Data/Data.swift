e99a57f69d4c6f98c08050b8ddfbc451812df714
Daweihao84 <dwsamurai84@gmail.com>
2023-12-16 20:19:01 +0800

    2023/12/16: 1. Add the collection View in main.storyboard. 2. Add the data files in swift.file. 3. Add the 21 photos in Asset.

---
+++ HW40_CollectionView/Data/Data.swift
@@ -0,0 +1,79 @@
+//
+//  Data.swift
+//  HW40_CollectionView
+//
+//  Created by Dawei Hao on 2023/12/16.
+//
+
+import Foundation
+
+struct Data {
+    let imageViewName: String
+    let locationURL:   String
+}
+
+
+let locationURL: [String] = [
+    // Sun Moon Lake 日月潭
+    "https://www.sunmoonlake.gov.tw/en",
+    
+    // Taipei 101
+    "https://www.taipei-101.com.tw/en/",
+    
+    // Taroko 太魯閣
+    "https://www-taroko-gov-tw.translate.goog/en/?_x_tr_sl=en&_x_tr_tl=zh-Hant&_x_tr_hl=zh-Hant&_x_tr_pto=tc",
+    
+    // Rural countryside 梯田
+    "https://ap-fftc-org-tw.translate.goog/article/3384?_x_tr_sl=en&_x_tr_tl=zh-Hant&_x_tr_hl=zh-Hant&_x_tr_pto=tc",
+    
+    // Ali-shan阿里山
+    "https://www.ali-nsa.net",
+
+    // Peng-Hu 澎湖
+    "https://eng-taiwan-net-tw.translate.goog/m1.aspx?sNo=0002125&_x_tr_sl=en&_x_tr_tl=zh-Hant&_x_tr_hl=zh-Hant&_x_tr_pto=tc",
+
+    // Lantern Festival 燈籠節
+    "https://eng.taiwan.net.tw/m1.aspx?sNo=0002019&lid=080923",
+
+    // Tea plantations
+    "https://eng.taiwan.net.tw/m1.aspx?sNo=0002124&id=5716",
+
+    // Tamsui 淡水
+    "https://www.taiwan.net.tw/m1.aspx?sNo=0001091&id=19",
+
+    // National Palace Museum 故宮
+    "https://www.npm.gov.tw/?l=2",
+
+    //  Shilin Night Market 士林夜市
+    "https://www-travel-taipei.translate.goog/en/attraction/details/1692?_x_tr_sl=en&_x_tr_tl=zh-Hant&_x_tr_hl=zh-Hant&_x_tr_pto=tc",
+
+    // Qingshui Cliffs 清水斷崖
+    "https://www.taroko.gov.tw/en/AttractionTrailContent.aspx?n=7899&sms=11274&s=116",
+
+    // Beitou Hot Springs 北投溫泉
+    "https://eng.taiwan.net.tw/m1.aspx?sNo=0002090&id=4029",
+
+    //  Lotus Pond 蓮花潭
+    "https://khh.travel/zh-tw/attractions/detail/11",
+
+    //  Chiang Kai-shek Memorial Hall 中正紀念堂
+    "https://www.cksmh.gov.tw/en/",
+
+    // Maokong Gondola 貓空纜車
+    "https://english.gondola.taipei",
+
+    // Fo Guang Shan Buddha Museum 佛光山佛陀紀念館
+    "https://www.fgsbmc.org.tw/en/index.aspx",
+
+    // Jiufen 九份
+    "https://eng-taiwan-net-tw.translate.goog/m1.aspx?sNo=0002091&id=290&_x_tr_sl=en&_x_tr_tl=zh-Hant&_x_tr_hl=zh-Hant&_x_tr_pto=tc",
+
+    // bustling Kaohsiung Harbor
+    "https://kh.twport.com.tw/en/cp.aspx?n=7A8A095986A78CE5",
+
+    // Ximending
+    "https://www.travel.taipei/zh-tw/attraction/details/1572",
+
+    // Yehliu Geopark
+    "https://eng-taiwan-net-tw.translate.goog/m1.aspx?sNo=0002091&id=155&_x_tr_sl=en&_x_tr_tl=zh-Hant&_x_tr_hl=zh-Hant&_x_tr_pto=tc"
+]
