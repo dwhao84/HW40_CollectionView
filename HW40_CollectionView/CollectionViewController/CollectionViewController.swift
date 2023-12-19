602b7063409690086a6efb5e5b0506b73ef22a8c
Daweihao84 <dwsamurai84@gmail.com>
2023-12-20 00:32:00 +0800

    2023/12/20: 1.Add the UICollectionViewDelegateFlowLayout in collectionView. 2. import minimumInteritemSpacingForSectionAt 3. import minimumLineSpacingForSectionAt

--- HW40_CollectionView/CollectionViewController/CollectionViewController.swift
+++ HW40_CollectionView/CollectionViewController/CollectionViewController.swift
@@ -11,6 +11,8 @@ class CollectionViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
 
+    private var images: [UIImage] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,8 +25,32 @@ class CollectionViewController: UIViewController {
         collectionView.delegate = self
         collectionView.dataSource = self
 
+        for _ in 0...20 {
+            images.append(UIImage(named: "pic01")!)
+            images.append(UIImage(named: "pic02")!)
+            images.append(UIImage(named: "pic03")!)
+            images.append(UIImage(named: "pic04")!)
+            images.append(UIImage(named: "pic05")!)
+            images.append(UIImage(named: "pic06")!)
+            images.append(UIImage(named: "pic07")!)
+            images.append(UIImage(named: "pic08")!)
+            images.append(UIImage(named: "pic09")!)
+            images.append(UIImage(named: "pic10")!)
+            images.append(UIImage(named: "pic11")!)
+            images.append(UIImage(named: "pic12")!)
+            images.append(UIImage(named: "pic13")!)
+            images.append(UIImage(named: "pic14")!)
+            images.append(UIImage(named: "pic15")!)
+            images.append(UIImage(named: "pic16")!)
+            images.append(UIImage(named: "pic17")!)
+            images.append(UIImage(named: "pic18")!)
+            images.append(UIImage(named: "pic19")!)
+            images.append(UIImage(named: "pic20")!)
+            images.append(UIImage(named: "pic20")!)
+        }
     }
 }
+
 extension CollectionViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         collectionView.deselectItem(at: indexPath, animated: true)
@@ -50,7 +76,22 @@ extension CollectionViewController: UICollectionViewDataSource {
 }
 
 extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+
+    // sizeForItemAt
+    // This means how many item will show in collectionView.
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 130, height: 130)
+        // 1.34 this value means to minus the space after we divide the three images space.
+        let size = (self.view.frame.width / 3) - 1.34
+        return CGSize(width: size, height: size)
+    }
+
+    // minimumLineSpacingForSectionAt
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
+    }
+
+    // minimumInteritemSpacingForSectionAt
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 2
     }
 }
