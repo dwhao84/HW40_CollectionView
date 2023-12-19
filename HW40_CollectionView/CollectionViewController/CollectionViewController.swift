03562460e3c93f03eb4968d68a7e8f48610ab3c2
Daweihao84 <dwsamurai84@gmail.com>
2023-12-19 23:57:16 +0800

    2023/12/19: Small Adjustment

--- HW40_CollectionView/CollectionViewController/CollectionViewController.swift
+++ HW40_CollectionView/CollectionViewController/CollectionViewController.swift
@@ -11,42 +11,46 @@ class CollectionViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
 
-    let pics: [Pics] = {
-        var pics = [Pics]()
-        for i in 0...20 {
-            let pic = Pics(image: "pics\(i)")
-            pics.append(pic)
-        }
-        return pics
-    }()
-
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 128, height: 128)
+        collectionView.collectionViewLayout = layout
+
+        collectionView.register(MyCollectionViewCell.nib(), forCellWithReuseIdentifier: MyCollectionViewCell.identifier)
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
 
     }
 }
 extension CollectionViewController: UICollectionViewDelegate {
-
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        print("You tapped me")
+    }
 }
-
 
 extension CollectionViewController: UICollectionViewDataSource {
 
 
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return pics.count
+        return 20
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
 
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseIdentifier, for: indexPath)
-        let pics = pics[indexPath.item]
-        cell.imageView?.image = UIImage(named: pics.image)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MyCollectionViewCell.identifier, for: indexPath) as! MyCollectionViewCell
 
+        cell.configure(with: UIImage(named: "pic01")!)
 
         return cell
     }
-    
+}
 
+extension CollectionViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 130, height: 130)
+    }
 }
--- HW40_CollectionView/CollectionViewController/MyCollectionViewCell.swift
+++ HW40_CollectionView/CollectionViewController/MyCollectionViewCell.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class MyCollectionViewCell: UICollectionViewCell {
 
+    static let identifier = "MyCollectionViewCell"
+
+    @IBOutlet var imageView: UIImageView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+
+    public func configure(with iamge: UIImage) {
+        imageView.image = UIImage(named:"pic01")
+    }
+
+    static func nib() -> UINib {
+        return UINib(nibName: "MyCollectionViewCell", bundle: nil)
     }
 
 }
