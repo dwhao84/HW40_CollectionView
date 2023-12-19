938bf83e08016debb798f51694bcc704413d9d2d
Daweihao84 <dwsamurai84@gmail.com>
2023-12-19 23:41:45 +0800

    2023/12/19: UI Components done.

---
+++ HW40_CollectionView/CollectionViewController/MyCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MyCollectionViewCell.swift
+//  HW40_CollectionView
+//
+//  Created by Dawei Hao on 2023/12/19.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
